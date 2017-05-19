6a9d5c28c1a0afc0550f83fb726d5897073d63fb
workanator <workanator@gmail.com>
2017-05-19 21:33:34 +0500

    Add curly style

---
+++ decorate/curly/doc.go
@@ -0,0 +1,12 @@
+/*
+Package curly defines curly bracket decoration style. The style introduces
+those rules.
+ - Tags are enclosed in `{` and `}`.
+ - Atrributes and modificators are concatenated with `_`.
+ - Multiple atrributes are delimited with `+`.
+
+ Examples:
+  `{yellow+bg_blue}` - Green text on blue bachground.
+  `{bold+bg_intesive_cyan}` - Bold text on high intensive cyan background.
+*/
+package curly
---
+++ decorate/curly/style.go
@@ -0,0 +1,50 @@
+package curly
+
+import (
+	"github.com/workanator/go-ataman/ansi"
+	"github.com/workanator/go-ataman/decorate"
+)
+
+var (
+	tagOpen              = decorate.NewMarker("{")
+	tagClose             = decorate.NewMarker("}")
+	attributeDelimiter   = decorate.NewMarker("+")
+	modificatorDelimiter = decorate.NewMarker("_")
+	autoReset            = true
+	attributes           = map[string]decorate.Attribute{
+		"-":          ansi.Reset,
+		"reset":      ansi.Reset,
+		"bold":       ansi.Bold, // Font attributes
+		"underscore": ansi.Underscore,
+		"underline":  ansi.Underscore,
+		"blink":      ansi.Blink,
+		"reverse":    ansi.Reverse,
+		"conceal":    ansi.Concealed,
+		"black":      ansi.Black, // Text colors
+		"red":        ansi.Red,
+		"green":      ansi.Green,
+		"yellow":     ansi.Yellow,
+		"blue":       ansi.Blue,
+		"magenta":    ansi.Magenta,
+		"cyan":       ansi.Cyan,
+		"white":      ansi.White,
+		"default":    ansi.Default,
+		"b":          ansi.Background, // Background modificator
+		"bg":         ansi.Background,
+		"background": ansi.Background,
+		"intensive":  ansi.Intensive, // High intensive modificator
+		"light":      ansi.Light,
+	}
+)
+
+// Style returns the basic decoration style
+func Style() decorate.Style {
+	return decorate.Style{
+		TagOpen:              tagOpen,
+		TagClose:             tagClose,
+		AttributeDelimiter:   attributeDelimiter,
+		ModificatorDelimiter: modificatorDelimiter,
+		AutoReset:            autoReset,
+		Attributes:           attributes,
+	}
+}

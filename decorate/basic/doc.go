02db471e0832049b560b121fea7ca8c25c4087ba
workanator <workanator@gmail.com>
2017-05-19 21:33:08 +0500

    Document basic style

---
+++ decorate/basic/doc.go
@@ -0,0 +1,11 @@
+/*
+Package basic defines basic decoration style. The style introduces those rules.
+ - Tags are enclosed in `<` and `>`.
+ - Atrributes and modificators are concatenated with `+`.
+ - Multiple atrributes are delimited with `,`.
+
+Examples:
+ `<yellow,bg+blue>` - Green text on blue bachground.
+ `<bold,bg+intesive+cyan>` - Bold text on high intensive cyan background.
+*/
+package basic

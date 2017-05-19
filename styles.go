024760048c85ecc774628e115c36ab96e745d1df
workanator <workanator@gmail.com>
2017-05-19 21:38:22 +0500

    Add curly style creation function

--- styles.go
+++ styles.go
@@ -3,9 +3,17 @@ package ataman
 import (
 	"github.com/workanator/go-ataman/decorate"
 	"github.com/workanator/go-ataman/decorate/basic"
+	"github.com/workanator/go-ataman/decorate/curly"
 )
 
-// BasicStyle returns teh basic decoration style.
+// BasicStyle returns the basic decoration style.
+// Quick example: `<bold,light+green,bg:black>`
 func BasicStyle() decorate.Style {
 	return basic.Style()
+}
+
+// CurlyStyle returns the curly brackets decoration style.
+// Quick example: `{bold+light_green+bg_black}`
+func CurlyStyle() decorate.Style {
+	return curly.Style()
 }
